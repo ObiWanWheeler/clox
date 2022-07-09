65cf8163b6092b860e4e819e0d21ce2f3b634c0d
ObiWanWheeler <charlie011204@gmail.com>
2022-07-09 10:29:24 +0100

    just about to add function table

--- src/compiler.c
+++ src/compiler.c
@@ -1,22 +1,176 @@
 #include "compiler.h"
+#include "chunk.h"
 #include "scanner.h"
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
 
-void compile(const char *source) {
-  initScanner(source);
+typedef struct {
+  Token current;
+  Token previous;
+  bool hadError;
+  bool panicMode;
+} Parser;
 
-  int line = -1;
+typedef enum {
+  PREC_NONE,
+  PREC_ASSIGNMENT, // =
+  PREC_OR,         // or
+  PREC_AND,        // and
+  PREC_EQUALITY,   // == !=
+  PREC_COMPARISON, // < > <= >=
+
+  PREC_TERM,   // + -
+  PREC_FACTOR, // * /
+
+  PREC_UNARY, // ! -
+
+  PREC_CALL, // . ()
+  PREC_PRIMARY
+} Precedence;
+
+Parser parser;
+Chunk *compilingChunk;
+
+static Chunk *currentChunk() { return compilingChunk; }
+
+static void errorAt(Token *token, const char *message) {
+  if (parser.panicMode)
+    return;
+  parser.panicMode = true;
+
+  fprintf(stderr, "[line %d] Error", token->line);
+
+  if (token->type == TOKEN_EOF) {
+    fprintf(stderr, "at end");
+  } else if (token->type == TOKEN_ERROR) {
+    // Nothing
+  } else {
+    fprintf(stderr, " at '%.*s'", token->length, token->start);
+  }
+
+  fprintf(stderr, ": %s\n", message);
+  parser.hadError = true;
+}
+
+static void errorAtCurrent(const char *message) {
+  errorAt(&parser.current, message);
+}
+
+static void error(const char *message) { errorAt(&parser.previous, message); }
+
+static void advance() {
+  parser.previous = parser.current;
 
   for (;;) {
-    Token token = scanToken();
-    if (token.line != line) {
-      printf("%4d ", token.line);
-      line = token.line;
-    } else {
-      printf("  | ");
-    }
-    printf("%2d '%.*s'\n", token.type, token.length, token.start);
+    if (parser.current.type != TOKEN_ERROR)
+      break;
 
-    if (token.type == TOKEN_EOF) break;
+    errorAtCurrent(parser.current.start);
   }
+}
+
+static void requireConsume(TokenType type, const char *message) {
+  if (parser.current.type == type) {
+    advance();
+    return;
+  }
+
+  errorAtCurrent(message);
+}
+
+static void emitByte(uint8_t byte) {
+  writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytePair(uint8_t byte1, uint8_t byte2) {
+  emitByte(byte1);
+  emitByte(byte2);
+}
+
+static void emitReturn() { emitByte(OP_RETURN); }
+
+static void endCompiler() { emitReturn(); }
+
+static uint8_t makeConstant(Value value) {
+  int constant = addConstant(currentChunk(), value);
+  if (constant > UINT8_MAX) {
+    error("Too many constants in one chunk");
+    return 0;
+  }
+
+  return (uint8_t)constant;
+}
+
+static void emitConstant(Value value) {
+  emitBytePair(OP_CONSTANT, makeConstant(value));
+}
+
+static void parsePrecendence(Precedence precendence) {}
+static void expression() { parsePrecendence(PREC_ASSIGNMENT); }
+
+static void binary() {
+  TokenType operatorType = parser.previous.type;
+  ParseRule *rule = getRule(operatorType);
+  parsePrecendence((Precedence)rule->precedence + 1);
+
+  switch (operatorType) {
+  case TOKEN_PLUS:
+    emitByte(OP_ADD);
+    break;
+  case TOKEN_MINUS:
+    emitByte(OP_SUBTRACT);
+    break;
+  case TOKEN_STAR:
+    emitByte(OP_MULTIPLY);
+    break;
+  case TOKEN_SLASH:
+    emitByte(OP_DIVIDE);
+    break;
+  default:
+    return; // Unreachable.
+  }
+}
+
+static void number() {
+  double value = strtod(parser.previous.start, NULL);
+  emitConstant(value);
+}
+
+static void grouping() {
+  expression();
+  requireConsume(TOKEN_RIGHT_PAREN,
+                 "Unmatched '('. Expect ')' after expression");
+}
+
+static void unary() {
+  TokenType operatorType = parser.previous.type;
+
+  // compile operand
+  parsePrecendence(PREC_UNARY);
+
+  // emit operator instruction
+  switch (operatorType) {
+  case (TOKEN_MINUS):
+    emitByte(OP_NEGATE);
+    break;
+  default:
+    return; // unreachable
+  }
+}
+
+bool compile(const char *source, Chunk *chunk) {
+  initScanner(source);
+  compilingChunk = chunk;
+
+  parser.hadError = false;
+  parser.panicMode = false;
+
+  advance();
+  expression();
+  requireConsume(TOKEN_EOF, "Expect end of expression.");
+
+  endCompiler();
+  return !parser.hadError;
 }
